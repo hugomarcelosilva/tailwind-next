820ea0bfebc18644f7bdbb8bee3dc26ee2f17d1f
Hugo Silva <hugo.marcelo@clevertech.biz>
2023-11-01 14:28:10 -0300

    feat: add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,17 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0px)' },
+        },
+      },
+
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
