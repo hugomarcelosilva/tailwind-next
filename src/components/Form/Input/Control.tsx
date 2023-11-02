1e86e8304a48638f9ec06c4cf8b2859df95a3141
Hugo Silva <hugo.marcelo@clevertech.biz>
2023-11-02 20:02:18 -0300

    style: customize dark theme for the sidebar

--- src/components/Form/Input/Control.tsx
+++ src/components/Form/Input/Control.tsx
@@ -5,7 +5,7 @@ type ControlProps = ComponentProps<'input'>
 export function Control(props: ControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:text-zinc-100 dark:placeholder-zinc-400"
       {...props}
     />
   )
